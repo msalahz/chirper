64f3eef87c2a82c94a607cabbe618fcab5597d78
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-05-04 21:42:20 +0200

    feat: Create middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+export default store => next => action => {
+  console.group(action.type)
+  console.log('The action:', action)
+  const returnValue = next(action)
+  console.log('The new state:', store.getState())
+  console.groupEnd()
+  return returnValue
+}
