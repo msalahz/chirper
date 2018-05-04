aefe20ff875b8f542dc63da17e6f7216e99d9add
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-05-05 00:53:38 +0200

    refactor: Add loadingBarReducer to store combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
 import users from './users'
 import tweets from './tweets'
 import authedUser from './authedUser'
 
-export default combineReducers({ users, tweets, authedUser })
+export default combineReducers({
+  users,
+  tweets,
+  authedUser,
+  loadingBar: loadingBarReducer
+})
