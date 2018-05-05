293876f8e21875b1f943ac6c7497e92285ce41a8
Mohammed Zaghloul <m.salahz86@gmail.com>
2018-05-05 21:31:41 +0200

    refactor: Add new Home & New Tweet routes, and add Nav component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import LoadingBar from 'react-redux-loading'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { func, bool } from 'prop-types'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import TweetForm from './TweetForm'
+import Nav from './Nav'
 
 class App extends Component {
+  static propTypes = {
+    loading: bool.isRequired,
+    dispatch: func.isRequired
+  }
+
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
 
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          {this.props.loading === true ? null : (
+            <div className="container">
+              <Nav />
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/new" component={TweetForm} />
+            </div>
+          )}
+        </Fragment>
+      </Router>
     )
   }
 }
